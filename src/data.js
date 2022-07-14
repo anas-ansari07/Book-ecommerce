eec2dcd564beac9d3fe4098d0663d06662468720
whitedevil-7 <anas792ansari@gmail.com>
2022-07-15 01:14:46 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,76 @@
+export const sliderItems = [
+    {
+      id: 1,
+      img: "images/books.png",
+      title: "SUMMER SALE",
+      desc: "DON'T COMPROMISE ON READING! GET FLAT 30% OFF ON IT.",
+      bg: "f5fafd",
+    },
+    {
+      id: 2,
+      img: "images/scifi.png",
+      title: "SCI-FI",
+      desc: "DON'T COMPROMISE ON READING! GET FLAT 30% OFF ON IT.",
+      bg: "fcf1ed",
+    },
+    {
+      id: 3,
+      img: "images/travel.png",
+      title: "TRAVEL",
+      desc: "DON'T COMPROMISE ON READING! GET FLAT 30% OFF ON IT.",
+      bg: "fbf0f4",
+    },
+  ];
+
+  export const categories = [
+    {
+      id: 1,
+      img: "images/biography.png",
+      title: "BIOGRAPHY",
+    },
+    {
+      id: 2,
+      img: "images/fant.png",
+      title: "SCI-FI & FANTASY",
+    },
+    {
+      id: 3,
+      img: "images/fiction.png",
+      title: "FICTION & LITERATURE",
+    },
+  ];
+
+  export const popularProducts = [
+    {
+      id:1,
+      img:"images/1.png",
+    },
+    {
+      id:2,
+      img:"images/2.png",
+    },
+    {
+      id:3,
+      img:"images/3.png",
+    },
+    {
+      id:4,
+      img:"images/4.png",
+    },
+    {
+      id:5,
+      img:"images/5.png",
+    },
+    {
+      id:6,
+      img:"images/6.png",
+    },
+    {
+      id:7,
+      img:"images/8.png",
+    },
+    {
+      id:8,
+      img:"images/7.png",
+    },
+  ]
